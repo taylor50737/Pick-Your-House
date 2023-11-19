0deba0d839bf44022788e9309496c1416f21cd98
Taylor Lo <taylorloph@gmail.com>
2023-11-19 13:58:00 -0500

    Added default layouts

--- resources/js/Pages/Index/Show.vue
+++ resources/js/Pages/Index/Show.vue
@@ -6,10 +6,3 @@
 <script setup>
 import { Link } from "@inertiajs/vue3";
 </script>
-
-<script>
-import MainLayout from "../../Layouts/MainLayout.vue";
-export default {
-    layout: MainLayout,
-};
-</script>
