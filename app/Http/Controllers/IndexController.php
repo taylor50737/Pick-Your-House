2c9a37b319d04bc0cefcbfbbaaeb70647d7ec9d7
Taylor Lo <taylorloph@gmail.com>
2023-11-19 23:02:11 -0500

    Database querying

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Listing;
 use Illuminate\Http\Request;
 
 class IndexController extends Controller
 {
     public function index() 
     {
+        // dd(Listing::all());
         return inertia(
             'Index/Index',
             [
