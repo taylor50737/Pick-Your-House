34ead490ac93c4e121b8839def94d8976edbf6db
Taylor Lo <taylorloph@gmail.com>
2023-11-23 23:04:07 -0500

    Laravel Route Names in Vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,7 @@
 import { createApp, h } from 'vue'
 import { createInertiaApp } from '@inertiajs/vue3'
 import MainLayout from '@/Layouts/MainLayout.vue'
+import { ZiggyVue } from 'ziggy'
 
 createInertiaApp({
   resolve: (name) => {
@@ -13,6 +14,7 @@ createInertiaApp({
   setup({ el, App, props, plugin }) {
     createApp({ render: () => h(App, props) })
       .use(plugin)
+      .use(ZiggyVue)
       .mount(el)
   },
 })
