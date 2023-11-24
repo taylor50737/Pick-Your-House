a2cb962ef94b9e48f020b7568804e5cec09c19bc
Taylor Lo <taylorloph@gmail.com>
2023-11-23 23:12:03 -0500

    Installing and Setting Up Tailwind CSS

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,6 +2,7 @@ import { createApp, h } from 'vue'
 import { createInertiaApp } from '@inertiajs/vue3'
 import MainLayout from '@/Layouts/MainLayout.vue'
 import { ZiggyVue } from 'ziggy'
+import '../css/app.css'
 
 createInertiaApp({
   resolve: (name) => {
