e1f8ea1a3b92ebf44f194b45b56fe89653068430
Taylor Lo <taylorloph@gmail.com>
2023-11-28 19:46:35 -0500

    Listing Page CSS

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -1,6 +1,6 @@
 <template>
   <header
-    class="border-b border-gray-200 dark:border-gray-700 bg-white dark:bg-gray-800 w-full"
+    class="border-b border-gray-200 dark:border-gray-700 bg-white dark:bg-gray-900 w-full"
   >
     <div class="container mx-auto">
       <nav class="p-4 flex items-center justify-between">
