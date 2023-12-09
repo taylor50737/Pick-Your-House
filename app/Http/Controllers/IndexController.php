9c4f49f65a38b09d19ac8e8101abee206f5da020
Taylor Lo <taylorloph@gmail.com>
2023-12-09 11:52:16 -0500

    Added Signing-In Logic

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -4,12 +4,13 @@ namespace App\Http\Controllers;
 
 use App\Models\Listing;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class IndexController extends Controller
 {
     public function index() 
     {
-        // dd(Listing::all());
+        dd(Auth::user());
         return inertia(
             'Index/Index',
             [
