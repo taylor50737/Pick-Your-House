a91e4c184dec892cbf62dda2fa6b8c64c56d1d4c
Taylor Lo <taylorloph@gmail.com>
2024-01-11 13:40:13 -0500

    Created One-to-many db relationship between listings and users

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -2,12 +2,21 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Listing extends Model
 {
     use HasFactory;
 
     protected $fillable = ['beds', 'baths', 'area', 'city', 'code', 'street', 'street_nr', 'price'];
+
+    public function owner(): BelongsTo
+    {
+        return $this->belongsTo(
+            \App\Models\User::class,
+            'by_user_id'
+        );
+    }
 }
