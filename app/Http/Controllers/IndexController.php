0c5c79737790b42d00f7d21955cc6d6d867a91c2
Taylor Lo <taylorloph@gmail.com>
2024-01-11 13:43:04 -0500

    One-to-many connecting models practice

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -10,7 +10,28 @@ class IndexController extends Controller
 {
     public function index() 
     {
+        // Uncomment the code below
+        // And write all the examples inside dd() call
+        // then visit http://127.0.0.1:8000 (or whatever php artisan serve suggests)
+        // to see the output
+        // dd(Listing::all());
+        // dd(Listing::where('beds', '>=', 4)->orderBy('price', 'asc')->first());
+        // Make sure to copy the code below, but WITHOUT comments, so uncomment it
+        // first (remove // from the beginning of 3 lines below, then copy)
+        // Listing::create([
+        //     'beds' => 2, 'baths' => 2, 'area' => 100, 'city' => 'North', 'street' => 'Tinker st', 'street_nr' => 20, 'code' => 'TS', 'price' => 200_000
+        // ])
+        // Listing::make([
+        //     'beds' => 2, 'baths' => 2, 'area' => 100, 'city' => 'North', 'street' => 'Tinker st', 'street_nr' => 20, 'code' => 'TS', 'price' => 200_000
+        // ])
+        // dd(Auth::user());
         // dd(Auth::check());
+
+        // dd(
+        //     Hash::make('password'),
+        //     '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
+        //     Hash::check('password', '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi')
+        // );
 
         return inertia(
             'Index/Index',
