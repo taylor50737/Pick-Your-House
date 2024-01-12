61f64a377ffb31e0116f225807216adc6a05340e
Taylor Lo <taylorloph@gmail.com>
2024-01-12 14:43:02 -0500

    filtering form default values

--- resources/js/Pages/Realtor/Index.vue
+++ resources/js/Pages/Realtor/Index.vue
@@ -1,6 +1,6 @@
 <template>
   <h1 class="text-3xl mb-4">Your Listings</h1>
-  <section><RealtorFilters /></section>
+  <section><RealtorFilters :filters="filters" /></section>
   <section class="grid grid-cols-1 lg:grid-cols-2 gap-2">
     <Box v-for="listing in listings" :key="listing.id">
       <div class="flex flex-col md:flex-row gap-2 md:items-center justify-between">
@@ -36,5 +36,9 @@ import Price from '@/Components/Price.vue'
 import Box from '@/Components/UI/Box.vue'
 import { Link } from '@inertiajs/vue3'
 import RealtorFilters from '@/Pages/Realtor/Index/Components/RealtorFilters.vue'
-defineProps({ listings: Array })
+
+defineProps({
+  listings: Array,
+  filters: Object,
+})
 </script>
