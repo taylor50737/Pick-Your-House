4ec35ae17f71231a32838af29d8cb64ecfdfeb66
Taylor Lo <taylorloph@gmail.com>
2024-01-12 13:57:20 -0500

    Realtor listings index page

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -7,16 +7,16 @@
         <div class="text-lg font-medium">
           <Link :href="route('listing.index')">Listings</Link>
         </div>
-        <div
-          class="text-xl text-indigo-600 dark:text-indigo-300 font-bold text-center"
-        >
+        <div class="text-xl text-indigo-600 dark:text-indigo-300 font-bold text-center">
           <Link :href="route('listing.index')">LaraZillow</Link>
         </div>
         <div v-if="user" class="flex items-center gap-4">
-          <div class="text-sm text-gray-500">{{ user.name }}</div>
-          <Link :href="route('listing.create')" class="btn-primary">
-            + New Listing
+          <Link class="text-sm text-gray-500" :href="route('realtor.listing.index')">
+            {{
+              user.name
+            }}
           </Link>
+          <Link :href="route('listing.create')" class="btn-primary"> + New Listing </Link>
           <div>
             <Link :href="route('logout')" method="delete" as="button">Logout</Link>
           </div>
@@ -30,7 +30,10 @@
   </header>
 
   <main class="container mx-auto p-4 w-full">
-    <div v-if="flashSuccess" class="mb-4 border rounded-md shadow-sm border-green-200 dark:border-green-800 bg-green-50 dark:bg-green-900 p-2">
+    <div
+      v-if="flashSuccess"
+      class="mb-4 border rounded-md shadow-sm border-green-200 dark:border-green-800 bg-green-50 dark:bg-green-900 p-2"
+    >
       {{ flashSuccess }}
     </div>
     <slot>Default</slot>
@@ -41,8 +44,6 @@
 import { computed } from 'vue'
 import { Link, usePage } from '@inertiajs/vue3'
 const page = usePage()
-const flashSuccess = computed(
-  () => page.props.flash.success,
-)
+const flashSuccess = computed(() => page.props.flash.success)
 const user = computed(() => page.props.user)
 </script>
