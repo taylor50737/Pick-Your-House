d5a1322253a07e5f0a6fe3557d86a61b12d9a563
Taylor Lo <taylorloph@gmail.com>
2024-01-12 14:46:34 -0500

    my account section pagination

--- resources/js/Pages/Realtor/Index.vue
+++ resources/js/Pages/Realtor/Index.vue
@@ -2,7 +2,7 @@
   <h1 class="text-3xl mb-4">Your Listings</h1>
   <section><RealtorFilters :filters="filters" /></section>
   <section class="grid grid-cols-1 lg:grid-cols-2 gap-2">
-    <Box v-for="listing in listings" :key="listing.id">
+    <Box v-for="listing in listings.data" :key="listing.id">
       <div class="flex flex-col md:flex-row gap-2 md:items-center justify-between">
         <div>
           <div class="xl:flex items-center gap-2">
@@ -27,6 +27,10 @@
       </div>
     </Box>
   </section>
+
+  <section v-if="listings.data.length" class="w-full flex justify-center mt-4 mb-4">
+    <Pagination :links="listings.links" />
+  </section>
 </template>
 
 <script setup>
@@ -34,11 +38,12 @@ import ListingAddress from '@/Components/ListingAddress.vue'
 import ListingSpace from '@/Components/ListingSpace.vue'
 import Price from '@/Components/Price.vue'
 import Box from '@/Components/UI/Box.vue'
+import Pagination from '@/Components/UI/Pagination.vue'
 import { Link } from '@inertiajs/vue3'
 import RealtorFilters from '@/Pages/Realtor/Index/Components/RealtorFilters.vue'
 
 defineProps({
-  listings: Array,
+  listings: Object,
   filters: Object,
 })
 </script>
