c358ec8eeff93a376a5cbc85dc8e0b6c3f883174
Taylor Lo <taylorloph@gmail.com>
2024-01-12 14:11:17 -0500

    Soft deleting listings

--- resources/js/Pages/Listing/Index/Components/Listing.vue
+++ resources/js/Pages/Listing/Index/Components/Listing.vue
@@ -1,39 +1,17 @@
 <template>
   <Box>
     <div>
-      <Link
-        :href="route('listing.show', {listing: listing.id})"
-      >
+      <Link :href="route('listing.show', { listing: listing.id })">
         <div class="flex items-center gap-1">
-          <Price
-            :price="listing.price"
-            class="text-2xl font-bold"
-          />
-          <div class="text-xs text-gray-500">
-            <Price :price="monthlyPayment" /> pm
-          </div>
+          <Price :price="listing.price" class="text-2xl font-bold" />
+          <div class="text-xs text-gray-500"><Price :price="monthlyPayment" /> pm</div>
         </div>
         <ListingSpace :listing="listing" class="text-lg" />
-        <ListingAddress
-          :listing="listing"
-          class="text-gray-500"
-        />
+        <ListingAddress :listing="listing" class="text-gray-500" />
       </Link>
     </div>
     <div>
-      <Link
-        :href="route('listing.edit', {listing: listing.id})"
-      >
-        Edit
-      </Link>
-    </div>
-    <div>
-      <Link
-        :href="route('listing.destroy', {listing: listing.id})"
-        method="DELETE" as="button"
-      >
-        Delete
-      </Link>
+      <Link :href="route('listing.edit', { listing: listing.id })"> Edit </Link>
     </div>
   </Box>
 </template>
@@ -46,8 +24,6 @@ import ListingSpace from '@/Components/ListingSpace.vue'
 import Box from '@/Components/UI/Box.vue'
 import { useMonthlyPayment } from '@/Composables/useMonthlyPayment'
 
-const props = defineProps({listing: Object})
-const { monthlyPayment } = useMonthlyPayment(
-  props.listing.price, 2.5, 25,
-)
+const props = defineProps({ listing: Object })
+const { monthlyPayment } = useMonthlyPayment(props.listing.price, 2.5, 25)
 </script>
