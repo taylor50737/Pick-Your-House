dcaac61809063f4cf58806c2693f59b291eb1cb3
Taylor Lo <taylorloph@gmail.com>
2024-01-13 12:19:31 -0500

    Added notification count

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -11,12 +11,21 @@
           <Link :href="route('listing.index')">LaraZillow</Link>
         </div>
         <div v-if="user" class="flex items-center gap-4">
+          <div class="text-gray-500 relative pr-2 py-2 text-lg">
+            🔔
+            <div
+              class="absolute right-0 top-0 w-5 h-5 bg-red-700 dark:bg-red-400 text-white font-medium border border-white dark:border-gray-900 rounded-full text-xs text-center"
+            >
+              {{ notificationCount }}
+            </div>
+          </div>
+
           <Link class="text-sm text-gray-500" :href="route('realtor.listing.index')">
-            {{
-              user.name
-            }}
+            {{ user.name }}
           </Link>
-          <Link :href="route('realtor.listing.create')" class="btn-primary"> + New Listing </Link>
+          <Link :href="route('realtor.listing.create')" class="btn-primary">
+            + New Listing
+          </Link>
           <div>
             <Link :href="route('logout')" method="delete" as="button">Logout</Link>
           </div>
@@ -46,4 +55,7 @@ import { Link, usePage } from '@inertiajs/vue3'
 const page = usePage()
 const flashSuccess = computed(() => page.props.flash.success)
 const user = computed(() => page.props.user)
+const notificationCount = computed(() =>
+  Math.min(page.props.value.user.notificationCount, 9),
+)
 </script>
