dad9cf19c850a7a6392a331846db92e5de5c8f1d
Taylor Lo <taylorloph@gmail.com>
2024-01-13 11:46:44 -0500

    Filtering out sold offer on listings page

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -27,6 +27,7 @@ class ListingController extends Controller
                 'filters' => $filters,
                 'listings' => Listing::mostRecent()
                     ->filter($filters)
+                    ->withoutSold()
                     ->paginate(10)
                     ->withQueryString()
             ]
