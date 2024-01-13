42417f7f44172c2ec80766a2221f95390b16c805
Taylor Lo <taylorloph@gmail.com>
2024-01-13 12:02:12 -0500

    UI improvement

--- resources/js/Pages/Listing/Show/Components/Offer.vue
+++ resources/js/Pages/Listing/Show/Components/Offer.vue
@@ -5,7 +5,7 @@
       <span
         v-if="offer.accepted_at"
         class="dark:bg-green-900 dark:text-green-200 bg-green-200 text-green-900 p-1 rounded-md uppercase ml-1"
-      >accepeted</span>
+      >accepted</span>
     </template>
 
     <section class="flex items-center justify-between">
@@ -20,7 +20,7 @@
       </div>
       <div>
         <Link
-          v-if="notSold"
+          v-if="!isSold"
           :href="route('realtor.offer.accept', { offer: offer.id })"
           class="btn-outline text-xs font-medium"
           as="button"
@@ -44,5 +44,4 @@ const props = defineProps({
 })
 const difference = computed(() => props.offer.amount - props.listingPrice)
 const madeOn = computed(() => new Date(props.offer.created_at).toDateString())
-const notSold = computed(() => !props.offer.accepted_at && !props.offer.rejected_at)
 </script>
