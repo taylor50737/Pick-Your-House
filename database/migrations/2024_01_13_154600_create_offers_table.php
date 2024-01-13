22e27bd650b9aabf6ee2b795f8961dc00fc15f22
Taylor Lo <taylorloph@gmail.com>
2024-01-13 10:52:03 -0500

    Offers data model and relations setup

---
+++ database/migrations/2024_01_13_154600_create_offers_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('offers', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+
+            $table->foreignIdFor(
+                \App\Models\Listing::class,
+                'listing_id'
+            )->constrained('listings');
+            $table->foreignIdFor(
+                \App\Models\User::class,
+                'bidder_id'
+            )->constrained('users');
+
+            $table->unsignedInteger('amount');
+
+            $table->timestamp('accepted_at')->nullable();
+            $table->timestamp('rejected_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('offers');
+    }
+};
