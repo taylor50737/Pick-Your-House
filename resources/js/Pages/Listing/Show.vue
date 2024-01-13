aad16ebdf70e54a116f195980cd47a4497fdec9a
Taylor Lo <taylorloph@gmail.com>
2024-01-13 10:55:53 -0500

    Offer widget frontend

--- resources/js/Pages/Listing/Show.vue
+++ resources/js/Pages/Listing/Show.vue
@@ -2,10 +2,7 @@
   <div class="flex flex-col-reverse md:grid md:grid-cols-12 gap-4">
     <Box class="md:col-span-7 flex items-center w-full">
       <div v-if="listing.images.length" class="grid grid-cols-2 gap-1">
-        <img
-          v-for="image in listing.images" :key="image.id"
-          :src="image.src"
-        />
+        <img v-for="image in listing.images" :key="image.id" :src="image.src" />
       </div>
       <div v-else class="w-full text-center font-medium text-gray-500">No images</div>
     </Box>
@@ -66,6 +63,8 @@
           </div>
         </div>
       </Box>
+
+      <MakeOffer :listing-id="listing.id" :price="listing.price" />
     </div>
   </div>
 </template>
@@ -76,6 +75,7 @@ import ListingAddress from '@/Components/ListingAddress.vue'
 import ListingSpace from '@/Components/ListingSpace.vue'
 import Box from '@/Components/UI/Box.vue'
 
+import MakeOffer from '@/Pages/Listing/Show/Components/MakeOffer.vue'
 import { ref } from 'vue'
 
 import { useMonthlyPayment } from '@/Composables/useMonthlyPayment'
@@ -88,7 +88,8 @@ const props = defineProps({
 })
 
 const { monthlyPayment, totalPaid, totalInterest } = useMonthlyPayment(
-  props.listing.price, interestRate, duration,
+  props.listing.price,
+  interestRate,
+  duration,
 )
-
 </script>
