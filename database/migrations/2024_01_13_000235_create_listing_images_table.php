26a5cb3eed0c1558866ceddf0bbcd49358ca3420
Taylor Lo <taylorloph@gmail.com>
2024-01-12 19:06:14 -0500

    File Data model

---
+++ database/migrations/2024_01_13_000235_create_listing_images_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('listing_images', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+
+            $table->string('filename');
+
+            $table->foreignIdFor(
+                \App\Models\Listing::class
+            )->constrained('listings');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('listing_images');
+    }
+};
