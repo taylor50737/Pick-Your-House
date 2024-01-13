ad8f373b2e8b58c086fab9d46c3f2951bb5096fa
Taylor Lo <taylorloph@gmail.com>
2024-01-13 11:59:18 -0500

    No Offer Making or Changing of Listings That Are Sold

--- app/Policies/ListingPolicy.php
+++ app/Policies/ListingPolicy.php
@@ -30,7 +30,11 @@ class ListingPolicy
      */
     public function view(?User $user, Listing $listing): bool
     {
-        return true;
+        if ($listing->by_user_id === $user?->id) {
+            return true;
+        }
+
+        return $listing->sold_at === null;
     }
 
     /**
@@ -46,7 +50,8 @@ class ListingPolicy
      */
     public function update(User $user, Listing $listing): bool
     {
-        return $user->id === $listing->by_user_id;
+        return $listing->sold_at === null
+            && ($user->id === $listing->by_user_id);
     }
 
     /**
